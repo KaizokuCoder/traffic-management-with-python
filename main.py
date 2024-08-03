a4450235e5fab833db9e4bda103cd2db8e0759e3
Paulo Lopes <81705881+KaizokuCoder@users.noreply.github.com>
2024-08-03 17:23:33 -0300

    Main Program

---
+++ main.py
@@ -0,0 +1,24 @@
+### ROTATÓRIA ###
+
+# Entradas da rotatória (carro/min)
+a1, a2, a3, a4 = 0
+
+# Saídas da rotatória (carro/min)
+b1, b2, b3, b4 = 0
+
+# Trechos da rotatória
+x1, x2, x3, x4 = 0
+
+# Capacidade de cada trecho
+CAPACITY = 0
+
+### SEMÁFORO ###
+
+# Tempo do ciclo do semáforo
+CYCLE_TIME = 0
+
+# Tempo do sinal verde
+green_time = 0
+
+# Fluxo de carros/min no sinal verde
+car_flow = 0
