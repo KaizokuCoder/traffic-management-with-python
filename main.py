4883411673984808d72ff082010d79768bd9bad6
Paulo Lopes <81705881+KaizokuCoder@users.noreply.github.com>
2024-08-18 23:31:29 -0300

    Updated main and simulation
    Read the comments on main! And test the code.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Main libraries
 import json
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 # Our files
 import escalation
@@ -8,7 +9,11 @@ import simulator
 # Image processing library
 from PIL import Image
 
+
 # !--> Search for the comments like this one though the code
+
+
+# Colors for terminal
 
 # Hard reset
 RESET = '\033[0m'
@@ -17,6 +22,7 @@ RESET = '\033[0m'
 RED = '\033[31m'
 GRAY = '\033[30m'
 BLUE = '\033[34m'
+GREEN = '\033[32m'
 YELLOW = '\033[33m'
 DARK_GRAY = '\033[90m'
 BRIGHT_YELLOW = '\033[93m'
@@ -25,7 +31,7 @@ BRIGHT_YELLOW = '\033[93m'
 COLOR_RESET = '\033[39m'
 
 # Roundway capacity
-CAPACITY = 50
+CAPACITY = 250
 
 def main():
   """
@@ -36,7 +42,7 @@ def main():
 
   # Open image with matplotlib
   
-  # !--> On this part we will explain the idea of the roundabout flow management
+  # !--> At this point, we will explain the idea of the roundabout flow management
   # !--> That's the whole purpose of the image below, you will see why later
   # !--> Remember to close the image to continue the program
 
@@ -45,74 +51,99 @@ def main():
   imgplot = plt.imshow(img)
   plt.show()
   
-  # !--> I put a "auto-complete" for the inputs values
-  # !--> I was bored of typing the same values over and over again
+  # !--> I put a "auto-complete" for the inputs values here
+  # !--> Because I was bored of typing the same values over and over again
   # !--> You can remove the comments and type the values manually
+  # !--> Also, I changed the variables names so that we fix our problem with the matrix
 
   print(f'Enter the input values {BLUE}(Blue){COLOR_RESET}, separating them with a space:')
   print(DARK_GRAY, end='')
-  # Ae, Be, Ce, De = [int(x) for x in input().split()]
-  Ae, Be, Ce, De =  [10, 20, 25, 15]
-  print(Ae, Be, Ce, De)
+  # Ae, Ce, Ee, Ge = [int(x) for x in input().split()]
+  Ae, Ce, Ee, Ge =  [27, 63, 48, 82]
+  print(Ae, Ce, Ee, Ge)
   print(COLOR_RESET, end='')
 
   print(f'\nEnter the output values {RED}(Red){COLOR_RESET}, separating them with a space:')
   print(DARK_GRAY, end='')
-  # As, Bs, Cs, Ds = [int(x) for x in input().split()]
-  As, Bs, Cs, Ds = [19, 11, 30, 10]
-  print(As, Bs, Cs, Ds)
+  # Bs, Ds, Fs, Hs = [int(x) for x in input().split()]
+  Bs, Ds, Fs, Hs = [35, 52, 68, 65]
+  print(Bs, Ds, Fs, Hs)
   print(COLOR_RESET, end='')
   print("\n")
 
   # Total number of inputs at the node = Total number of outputs at the node
-  if (Ae + Be + Ce + De != As + Bs + Cs + Ds):
+  if (Ae + Ce + Ee + Ge != Bs + Ds + Fs + Hs):
     print(RED + '\nThe total number of inputs is different from the total number of outputs')
     print(COLOR_RESET, end='')
     return 1
+  
+  # !--> Here. From now on I changed the way we built the matrix
+  # !--> Now it has 8 nodes... 😢
+  # !--> We also will need a new image for representation of the roundabout
 
-  # A: X1 + Ae = As + X2 => X1 - X2 = As - Ae
-  # B: X2 + Be = Bs + X3 => X2 - X3 = Bs - Be
-  # C: X3 + Ce = Cs + X4 => X3 - X4 = Cs - Ce
-  # D: X4 + De = Ds + X1 => X4 - X1 = Ds - De
+  # Equations:
 
-  # Storing the results in a variable
-  A = As - Ae
-  B = Bs - Be
-  C = Cs - Ce
-  D = Ds - De
+  # A: X8 + Ae = X1 ==> X8 - X1 = -Ae
+  # B: X1 = Bs + X2 ==> X1 - X2 = Bs
+  # C: X2 + Ce = X3 ==> X2 - X3 = -Ce
+  # D: X3 = Cs + X4 ==> X3 - X4 = Ds
+  # E: X4 + Ee = X5 ==> X4 - X5 = -Ee
+  # F: X5 = Ds + X6 ==> X5 - X6 = Fs
+  # G: X6 + Ge = X7 ==> X6 - X7 = -Ge
+  # H: X7 = Fs + X8 ==> X7 - X8 = Hs
 
   # Creating the matrix
 
-  flow_matrix = [[1, -1, 0, 0, A],
-                 [0, 1, -1, 0, B],
-                 [0, 0, 1, -1, C],
-                 [-1, 0, 0, 1, D]]
+  flow_matrix = [[-1,  0,  0,  0,  0,  0,  0,  1, -Ae],
+                 [ 1, -1,  0,  0,  0,  0,  0,  0,  Bs],
+                 [ 0,  1, -1,  0,  0,  0,  0,  0, -Ce],
+                 [ 0,  0,  1, -1,  0,  0,  0,  0,  Ds],
+                 [ 0,  0,  0,  1, -1,  0,  0,  0, -Ee],
+                 [ 0,  0,  0,  0,  1, -1,  0,  0,  Fs],
+                 [ 0,  0,  0,  0,  0,  1, -1,  0, -Ge],
+                 [ 0,  0,  0,  0,  0,  0,  1, -1,  Hs],]
   
   # !--> Now we are using the escalation.py file to solve the matrix
   # !--> It basically show step by step the solution of the matrix
-  # !--> It also prints the matrix on terminal, but we can change later if you all didnt like
+  # !--> It also prints the matrix on terminal step by step
+  # !--> We will need to remove that print later, because its huge and annoying
 
   escalation.toLrfe(flow_matrix)
 
-  solution_1 = flow_matrix[0][4]
-  solution_2 = flow_matrix[1][4]
-  solution_3 = flow_matrix[2][4]
-  # solution_4 must be 0
+  solution_1 = flow_matrix[0][8]
+  solution_2 = flow_matrix[1][8]
+  solution_3 = flow_matrix[2][8]
+  solution_4 = flow_matrix[3][8]
+  solution_5 = flow_matrix[4][8]
+  solution_6 = flow_matrix[5][8]
+  solution_7 = flow_matrix[6][8]
+  # solution_8 must be 0
 
-  # [[1, -1, 0, 0, A + B + C    ],
-  #  [0, 1, -1, 0, B + C        ],
-  #  [0, 0, 1, -1, C            ],
-  #  [0, 0, 0,  1, D + A + B + C]]
+  # !--> Some explanation about the results
 
-  # A + B + C + D must be 0
-  # ou seja: As - Ae + Bs - Be + Cs - Ce + Ds - De = 0
+  # [[ 1,  0,  0,  0,  0,  0,  0, -1, Ae                                    ],
+  #  [ 0,  1,  0,  0,  0,  0,  0, -1, -Bs + Ae                              ],
+  #  [ 0,  0,  1,  0,  0,  0,  0, -1, Ce - Bs + Ae                          ],
+  #  [ 0,  0,  0,  1,  0,  0,  0, -1, -Ds + Ce - Bs + Ae                    ],
+  #  [ 0,  0,  0,  0,  1,  0,  0, -1, Ee - Ds + Ce - Bs + Ae                ],
+  #  [ 0,  0,  0,  0,  0,  1,  0, -1, -Fs + Ee - Ds + Ce - Bs + Ae          ],
+  #  [ 0,  0,  0,  0,  0,  0,  1, -1, Ge - Fs + Ee - Ds + Ce - Bs + Ae      ],
+  #  [ 0,  0,  0,  0,  0,  0,  0,  0, -Hs + Ge - Fs + Ee - Ds + Ce - Bs + Ae]]
 
-  # X1 = A + B + C + X4
-  # X2 = B + C + X4
-  # X3 = C + X4
-  # X4 = X4 -> Free variable
+  # Entries - Exits must be 0
+  # In other words: Ae + Be + Ce + De - As - Bs - Cs - Ds = 0
+
+  # X1 = Ae + X8
+  # X2 = -Bs + Ae + X8
+  # X3 = Ce - Bs + Ae + X8
+  # X4 = -Ds + Ce - Bs + Ae + X8
+  # X5 = Ee - Ds + Ce - Bs + Ae + X8
+  # X6 = -Fs + Ee - Ds + Ce - Bs + Ae + X8
+  # X7 = Ge - Fs + Ee - Ds + Ce - Bs + Ae + X8
+  # X8 = X8
+
   
-  # X1 + X2 + X3 + X4 <= CAPACITY (less or equal the roundway capacity)
+  # X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 <= CAPACITY (less or equal the roundway capacity)
 
   """
   Parte 1 - Predicting the flow
@@ -120,42 +151,39 @@ def main():
   
   # Finding the lower bound
 
-  lower_bound = 0
-
-  if (solution_3 < solution_2 and solution_3 < solution_1):
-    lower_bound = solution_3
-  elif (solution_2 < solution_1):
-    lower_bound = solution_2
-  else:
-    lower_bound = solution_1
-
+  lower_bound = min(solution_1, solution_2, solution_3, solution_4, solution_5, solution_6, solution_7)
   
   print('\033[01m' + '\nLet\'s predict the flow of cars in the roundabout' + RESET)
-  print(f'Type the value of X4: ')
+  print(f'Type the value of X8: ')
 
   print(DARK_GRAY, end='')
-  X4 = int(input())
+  X8 = int(input())
   print(COLOR_RESET, end='')
 
-  while X4 < lower_bound:
-    print(f'Type the value of X4 {YELLOW}(greater than {lower_bound}){COLOR_RESET}: ')
+  while X8 < (-lower_bound):
+    print(f'Type the value of X8 {YELLOW}(greater than {-lower_bound}){COLOR_RESET}: ')
     print(DARK_GRAY, end='')
-    X4 = int(input())
+    X8 = int(input())
     print(COLOR_RESET, end='')
 
   # Calculating the flow of cars
-  X1 = solution_1 + X4
-  X2 = solution_2 + X4
-  X3 = solution_3 + X4
+  X1 = solution_1 + X8
+  X2 = solution_2 + X8
+  X3 = solution_3 + X8
+  X4 = solution_4 + X8
+  X5 = solution_5 + X8
+  X6 = solution_6 + X8
+  X7 = solution_7 + X8
 
-  print(f"\nThe flow of cars are: X1: {DARK_GRAY}{X1}{COLOR_RESET} X2: {DARK_GRAY}{X2}{COLOR_RESET} X3: {DARK_GRAY}{X3}{COLOR_RESET} X4: {DARK_GRAY}{X4}{COLOR_RESET}\n")
+
+  print(f"\nThe flow of cars are:\nX1: {DARK_GRAY}{X1}{COLOR_RESET}\nX2: {DARK_GRAY}{X2}{COLOR_RESET}\nX3: {DARK_GRAY}{X3}{COLOR_RESET}\nX4: {DARK_GRAY}{X4}{COLOR_RESET}\nX5: {DARK_GRAY}{X5}{COLOR_RESET}\nX6: {DARK_GRAY}{X6}{COLOR_RESET}\nX7: {DARK_GRAY}{X7}{COLOR_RESET}\nX8: {DARK_GRAY}{X8}{COLOR_RESET}\n")
 
 
   """
   Parte 2 - Verify if the cars flow is greater than the capacity
   """
     
-  if (X1 + X2 + X3 + X4 > CAPACITY):
+  if (X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 > CAPACITY):
     print(RED + 'The flow of cars is greater than the capacity of the roundabout')
     print(BRIGHT_YELLOW + 'A traffic light is needed to control the flow!')
     print(COLOR_RESET, end='')
@@ -168,19 +196,38 @@ def main():
   Parte 3 - Simulation :)
   """
 
-  # !--> So this is kind of a surprise, hehehe.
+  # !--> So this is the new part of the code...
   # !--> Run and see the results!
   # !--> Give feedback later please!
-  # !--> I will also add more features later, like adding a traffic light to control the flow.
+  # !--> The purpose of this part is to show the theory in practice.
+  # !--> Hopefuly it helps with the explanation and how it works.
+  # !--> I just added the traffic light system, guess it works... kinda... 😅
 
   print(BLUE,"\n\n\tStart Simulation?\n\n\t(y/n): ",COLOR_RESET, end='')
   print(DARK_GRAY, end='')
-  start = input()
+  run = input()
   print(COLOR_RESET, end='')
 
-  if start == 'y':
-    values = [[Ae, Be, Ce, De], [As, Bs, Cs, Ds], [X1, X2, X3, X4]]
-    simulator.run(values, CAPACITY)
+  while run == 'y':
+    print(f"\nType the roads where you want to place the first cycle of traffic lights.\nJust the entries {BLUE}(Blue){COLOR_RESET} or empty.\n: ", end="")
+    print(DARK_GRAY, end='')
+    traffic_lights1 = input().split()
+    print(COLOR_RESET, end='')
+    print(f"\nType the roads where you want to place the second cycle of traffic lights\nJust the entries {BLUE}(Blue){COLOR_RESET} or empty.\n{YELLOW}Cant be the same as before.{COLOR_RESET}\n: ", end="")
+    print(DARK_GRAY, end='')
+    traffic_lights2 = input().split()
+    print(COLOR_RESET, end='')
+
+    print(GREEN,"\nRunning simulation...")
+  
+    values = [[Ae, Ce, Ee, Ge], [Bs, Ds, Fs, Hs]]
+    simulator.run(values, CAPACITY, X8, traffic_lights1, traffic_lights2)
+
+    print(BLUE,"\n\n\tRun again?\n\n\t(y/n): ",COLOR_RESET, end='')
+    print(DARK_GRAY, end='')
+    run = input()
+    print(COLOR_RESET, end='')
+
 
   return 0
 
--- simulator.py
+++ simulator.py
@@ -3,154 +3,233 @@ import pygame as pg
 # COLORS
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+RED = (255, 32, 78)
 BLUE = (0, 0, 255)
-AQUA = (0, 255, 255)
-GREEN = (0, 255, 0)
+GREEN = (22, 255, 0)
 YELLOW = (255, 255, 0)
+ORANGE = (255, 143, 0)
+LIGHT_BLUE = (0, 121, 255)
+LIGHT_PURPLE = (124, 0, 254)
 
-def create_arrow_surface(color):
-    arrow_surface = pg.Surface((20, 125), pg.SRCALPHA)
-    arrow_points = [(10, 0), (20, 20), (12, 20), (12, 125), (8, 125), (8, 20), (0, 20)]
+
+def create_arrow_surface(color, size='normal'):
+    if size == 'normal':
+        arrow_surface = pg.Surface((20, 110), pg.SRCALPHA)
+        arrow_points = [(10, 0), (20, 20), (12.5, 20), (12.5, 110), (7.5, 110), (7.5, 20), (0, 20)]
+    elif size == 'small':
+        arrow_surface = pg.Surface((20, 100), pg.SRCALPHA)
+        arrow_points = [(10, 0), (20, 20), (12.5, 20), (12.5, 100), (7.5, 100), (7.5, 20), (0, 20)]
+
     pg.draw.polygon(arrow_surface, color, arrow_points)
     return arrow_surface
+
 
 def draw_arrow(screen, arrow_surface, pos, angle):
     rotated_arrow = pg.transform.rotate(arrow_surface, angle)
     arrow_rect = rotated_arrow.get_rect(center=pos)
     screen.blit(rotated_arrow, arrow_rect.topleft)
 
+
 def draw_circle(screen, pos, radius, color):
     pg.draw.circle(screen, color, pos, radius)
+
 
 def render_text(screen, font, text, pos, color):
     text_surface = font.render(text, True, color)
     screen.blit(text_surface, pos)
 
-def run(values, CAPACITY):
+def fill_arrow(surface, color):
+    mask = pg.mask.from_surface(surface)
+    for x in range(surface.get_width()):
+        for y in range(surface.get_height()):
+            if mask.get_at((x, y)):
+                surface.set_at((x, y), color)
+
+
+def run(values, CAPACITY, X8, traffic_lights1, traffic_lights2):
     # Initialize Pygame
     pg.init()
-    screen = pg.display.set_mode((1000, 700))
+    screen = pg.display.set_mode((1250, 700))
+    pg.display.set_caption("Simulation with FPS Control")
+    clock = pg.time.Clock()
+    running = True
+    FPS = 60  # Set the desired FPS
+    frame_counter = 0
 
     # Initialize values
-    Ae, Be, Ce, De = values[0]
-    As, Bs, Cs, Ds = values[1]
-    X1, X2, X3, X4 = values[2]
-
-    current_flux = X1 + X2 + X3 + X4
-    total_entries = Ae + Be + Ce + De
-    total_exits = As + Bs + Cs + Ds
+    Ae, Ce, Ee, Ge = values[0]
+    Bs, Ds, Fs, Hs = values[1]
 
     # Initialize font
     pg.font.init()
     font_big = pg.font.Font(None, 50)  # None for default font
     font_normal = pg.font.Font(None, 30)
-    font_small = pg.font.Font(None, 20)
+    font_small = pg.font.Font(None, 24)
 
     # Create arrow surfaces
-    arrow_north_in = create_arrow_surface(GREEN)
-    arrow_north_out = create_arrow_surface(YELLOW)
-    arrow_east_in = create_arrow_surface(GREEN)
-    arrow_east_out = create_arrow_surface(YELLOW)
-    arrow_south_in = create_arrow_surface(GREEN)
-    arrow_south_out = create_arrow_surface(YELLOW)
-    arrow_west_in = create_arrow_surface(GREEN)
-    arrow_west_out = create_arrow_surface(YELLOW)
+    arrow_north_in, arrow_north_out, arrow_east_in, arrow_east_out, arrow_south_in, arrow_south_out, arrow_west_in, arrow_west_out = [create_arrow_surface(LIGHT_BLUE, 'small') if i % 2 == 0 else create_arrow_surface(ORANGE, 'small') for i in range(8)]
 
-    arrow_ab = create_arrow_surface(WHITE)
-    arrow_bc = create_arrow_surface(WHITE)
-    arrow_cd = create_arrow_surface(WHITE)
-    arrow_da = create_arrow_surface(WHITE)
+    arrow_surfaces = [create_arrow_surface(WHITE) for _ in range(8)]
+    arrow_surfaces = [pg.transform.scale(arrow_surface, (arrow_surface.get_width(), int(arrow_surface.get_height() * 1.2))) for arrow_surface in arrow_surfaces]
+    arrow_ab, arrow_bc, arrow_cd, arrow_de, arrow_ef, arrow_fg, arrow_gh, arrow_ha = arrow_surfaces
 
-        
     # Main loop
-    running = True
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
 
         screen.fill(BLACK)
+        
+        Ae_aux, Ce_aux, Ee_aux, Ge_aux = Ae, Ce, Ee, Ge
 
-        # Draw Arrows
+        if set(traffic_lights1) & set(traffic_lights2):
+            print("Cycle 2 cannot have the same roads as Cycle 1. Please try again.")
+            traffic_lights2 = []
+        else:
+            if (frame_counter//600) % 2 == 0:
+                traffic_lights_red = traffic_lights1
+                traffic_lights_green = traffic_lights2
+            else:
+                traffic_lights_red = traffic_lights2
+                traffic_lights_green = traffic_lights1
+            if 'Ae' in traffic_lights_red:
+                Ae_aux = 0
+                fill_arrow(arrow_north_in, RED)
+            else:
+                fill_arrow(arrow_north_in, GREEN)
+            if 'Ce' in traffic_lights_red:
+                Ce_aux = 0
+                fill_arrow(arrow_east_in, RED)
+            else:
+                fill_arrow(arrow_east_in, GREEN)
+            if 'Ee' in traffic_lights_red:
+                Ee_aux = 0
+                fill_arrow(arrow_south_in, RED)
+            else:
+                fill_arrow(arrow_south_in, GREEN)
+            if 'Ge' in traffic_lights_red:
+                Ge_aux = 0
+                fill_arrow(arrow_west_in, RED)
+            else:
+                fill_arrow(arrow_west_in, GREEN)
+        
+        
+        # Update Values
+        X1 = Ae_aux + X8
+        X2 = -Bs + Ae_aux + X8
+        X3 = Ce_aux - Bs + Ae_aux + X8
+        X4 = -Ds + Ce_aux - Bs + Ae_aux + X8
+        X5 = Ee_aux - Ds + Ce_aux - Bs + Ae_aux + X8
+        X6 = -Fs + Ee_aux - Ds + Ce_aux - Bs + Ae_aux + X8
+        X7 = Ge_aux - Fs + Ee_aux - Ds + Ce_aux - Bs + Ae_aux + X8
 
-        # North
-        draw_arrow(screen, arrow_north_out, (635, 125), 0)
-        draw_arrow(screen, arrow_north_in, (595, 125), 180)
-        # East
-        draw_arrow(screen, arrow_east_in, (885, 320), 90)
-        draw_arrow(screen, arrow_east_out, (885, 360), -90)
-        # South
-        draw_arrow(screen, arrow_south_in, (635, 580), 0)
-        draw_arrow(screen, arrow_south_out, (595, 580), 180)
-        # West
-        draw_arrow(screen, arrow_west_in, (345, 370), -90)
-        draw_arrow(screen, arrow_west_out, (345, 330), 90)
-        # Connections
-        draw_arrow(screen, arrow_ab, (715, 280), -125)
-        draw_arrow(screen, arrow_bc, (715, 430), 125)
-        draw_arrow(screen, arrow_cd, (515, 430), 55)
-        draw_arrow(screen, arrow_da, (525, 280), -55)
+        current_flux = X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8
+        total_entries = Ae + Ce + Ee + Ge
+        total_exits = Bs + Ds + Fs + Hs
+        overloaded = current_flux > CAPACITY # True if the roundway is overloaded
 
-        # Render text
+        # Draw Graph
 
         # Nodes
-        render_text(screen, font_big, 'A', (605, 200), WHITE)
-        render_text(screen, font_big, 'B', (785, 330), WHITE)
-        render_text(screen, font_big, 'C', (605, 480), WHITE)
-        render_text(screen, font_big, 'D', (425, 340), WHITE)
+        render_text(screen, font_big, 'A', (600, 130), YELLOW)
+        render_text(screen, font_big, 'B', (800, 130), YELLOW)
+        render_text(screen, font_big, 'C', (950, 230), YELLOW)
+        render_text(screen, font_big, 'D', (950, 430), YELLOW)
+        render_text(screen, font_big, 'E', (800, 530), YELLOW)
+        render_text(screen, font_big, 'F', (600, 530), YELLOW)
+        render_text(screen, font_big, 'G', (455, 430), YELLOW)
+        render_text(screen, font_big, 'H', (455, 230), YELLOW)
 
         # Edges - In and Out
         # North
-        render_text(screen, font_normal, 'Ae=%.2f' % Ae, (505, 110), GREEN) 
-        render_text(screen, font_normal, 'As=%.2f' % As, (650, 110), YELLOW)
+        draw_arrow(screen, arrow_north_in, (612, 70), 180)
+        draw_arrow(screen, arrow_north_out, (812, 70), 0)
         # East
-        render_text(screen, font_normal, 'Be=%.2f' % Be, (855, 290), GREEN)
-        render_text(screen, font_normal, 'Bs=%.2f' % Bs, (855, 375), YELLOW)
+        draw_arrow(screen, arrow_east_in, (1035, 245), 90)
+        draw_arrow(screen, arrow_east_out, (1035, 445), -90)
         # South
-        render_text(screen, font_normal, 'Ce=%.2f' % Ce, (650, 565), GREEN)
-        render_text(screen, font_normal, 'Cs=%.2f' % Cs, (505, 565), YELLOW)
+        draw_arrow(screen, arrow_south_in, (812, 620), 0)
+        draw_arrow(screen, arrow_south_out, (612, 620), 180)
         # West
-        render_text(screen, font_normal, 'De=%.2f' % De, (310, 385), GREEN)
-        render_text(screen, font_normal, 'Ds=%.2f' % Ds, (310, 300), YELLOW)
+        draw_arrow(screen, arrow_west_in, (395, 445), 270)
+        draw_arrow(screen, arrow_west_out, (395, 245), 90)
 
         # Edges - Connections
-        render_text(screen, font_normal, 'X1=%.2f' % X1, (715, 250), WHITE)
-        render_text(screen, font_normal, 'X2=%.2f' % X2, (725, 440), WHITE)
-        render_text(screen, font_normal, 'X3=%.2f' % X3, (520, 410), WHITE)
-        render_text(screen, font_normal, 'X4=%.2f' % X4, (520, 300), WHITE)
+        draw_arrow(screen, arrow_ab, (715, 145), -90)
+        draw_arrow(screen, arrow_bc, (890, 190), -125)
+        draw_arrow(screen, arrow_cd, (962, 340), 180)
+        draw_arrow(screen, arrow_de, (885, 495), 125)
+        draw_arrow(screen, arrow_ef, (710, 548), 90)
+        draw_arrow(screen, arrow_fg, (540, 495), 55)
+        draw_arrow(screen, arrow_gh, (467, 345), 0)
+        draw_arrow(screen, arrow_ha, (538, 195), -55)
+
+        # Edges Values
+        render_text(screen, font_small, 'X1: %d' % X1, (680, 110), WHITE)
+        render_text(screen, font_small, 'X2: %d' % X2, (895, 170), WHITE)
+        render_text(screen, font_small, 'X3: %d' % X3, (980, 320), WHITE)
+        render_text(screen, font_small, 'X4: %d' % X4, (900, 500), WHITE)
+        render_text(screen, font_small, 'X5: %d' % X5, (690, 560), WHITE)
+        render_text(screen, font_small, 'X6: %d' % X6, (485, 510), WHITE)
+        render_text(screen, font_small, 'X7: %d' % X7, (395, 345), WHITE)
+        render_text(screen, font_small, 'X8: %d' % X8, (480, 170), WHITE)
+
+        # Edges In and Out Values
+        render_text(screen, font_small, 'Ae: %d' % Ae_aux, (620, 60), RED if 'Ae' in traffic_lights_red else GREEN)
+        render_text(screen, font_small, 'Bs: %d' % Bs, (825, 60), ORANGE)
+        render_text(screen, font_small, 'Ce: %d' % Ce_aux, (1025, 215), RED if 'Ce' in traffic_lights_red else GREEN)
+        render_text(screen, font_small, 'Ds: %d' % Ds, (1005, 415), ORANGE)
+        render_text(screen, font_small, 'Ee: %d' % Ee_aux, (820, 615), RED if 'Ee' in traffic_lights_red else GREEN)
+        render_text(screen, font_small, 'Fs: %d' % Fs, (620, 615), ORANGE)
+        render_text(screen, font_small, 'Ge: %d' % Ge_aux, (360, 415), RED if 'Ge' in traffic_lights_red else GREEN)
+        render_text(screen, font_small, 'Hs: %d' % Hs, (370, 215), ORANGE)
+
+
 
         # Show Data
-        render_text(screen, font_normal, 'Roundway Status', (10, 10), AQUA)
-        render_text(screen, font_small, 'CAPACITY: %.2f' % CAPACITY, (10, 40), WHITE)
-        render_text(screen, font_small, 'FLUX INSIDE: %.2f' % current_flux, (10, 60), WHITE)
-        if (X1 + X2 + X3 + X4) > CAPACITY:
-            render_text(screen, font_small, 'ROUNDWAY OVERLOADED', (10, 80), RED)
+        render_text(screen, font_normal, 'Roundway Status', (10, 10), GREEN)
+        render_text(screen, font_small, 'capacity: %d' % CAPACITY, (10, 40), WHITE)
+        render_text(screen, font_small, 'flux inside: %d' % current_flux, (10, 60), WHITE)
+        if (overloaded):
+            render_text(screen, font_small, 'Roundway Overloaded', (10, 80), RED)
         else:
-            render_text(screen, font_small, 'ROUNDWAY ON NORMAL CONDITIONS', (10, 80), GREEN)
+            render_text(screen, font_small, 'Roundway Stable', (10, 80), LIGHT_BLUE)
         
-        render_text(screen, font_normal, 'Entries', (10, 110), AQUA)
-        render_text(screen, font_small, 'Ae: %.2f %%' % ((Ae/total_entries)*100), (10, 140), GREEN)
-        render_text(screen, font_small, 'Be: %.2f %%' % ((Be/total_entries)*100), (10, 160), GREEN)
-        render_text(screen, font_small, 'Ce: %.2f %%' % ((Ce/total_entries)*100), (10, 180), GREEN)
-        render_text(screen, font_small, 'De: %.2f %%' % ((De/total_entries)*100), (10, 200), GREEN)
+        render_text(screen, font_normal, 'Entries', (10, 110), GREEN)
+        render_text(screen, font_small, 'Ae: %.2f %%' % ((Ae/total_entries)*100), (10, 140), WHITE)
+        render_text(screen, font_small, 'Ce: %.2f %%' % ((Ce/total_entries)*100), (10, 160), WHITE)
+        render_text(screen, font_small, 'Ee: %.2f %%' % ((Ee/total_entries)*100), (10, 180), WHITE)
+        render_text(screen, font_small, 'Ge: %.2f %%' % ((Ge/total_entries)*100), (10, 200), WHITE)
 
-        render_text(screen, font_normal, 'Exits', (10, 230), AQUA)
-        render_text(screen, font_small, 'As: %.2f %%' % ((As/total_exits)*100), (10, 260), YELLOW)
-        render_text(screen, font_small, 'Bs: %.2f %%' % ((Bs/total_exits)*100), (10, 280), YELLOW)
-        render_text(screen, font_small, 'Cs: %.2f %%' % ((Cs/total_exits)*100), (10, 300), YELLOW)
-        render_text(screen, font_small, 'Ds: %.2f %%' % ((Ds/total_exits)*100), (10, 320), YELLOW)
+        render_text(screen, font_normal, 'Exits', (10, 230), GREEN)
+        render_text(screen, font_small, 'Bs: %.2f %%' % ((Bs/total_exits)*100), (10, 260), WHITE)
+        render_text(screen, font_small, 'Ds: %.2f %%' % ((Ds/total_exits)*100), (10, 280), WHITE)
+        render_text(screen, font_small, 'Fs: %.2f %%' % ((Fs/total_exits)*100), (10, 300), WHITE)
+        render_text(screen, font_small, 'Hs: %.2f %%' % ((Hs/total_exits)*100), (10, 320), WHITE)
 
-        render_text(screen, font_normal, 'Connections', (10, 350), AQUA)
+        render_text(screen, font_normal, 'Connections', (10, 350), GREEN)
         render_text(screen, font_small, 'X1: %.2f %%' % ((X1/current_flux)*100), (10, 380), WHITE)
         render_text(screen, font_small, 'X2: %.2f %%' % ((X2/current_flux)*100), (10, 400), WHITE)
         render_text(screen, font_small, 'X3: %.2f %%' % ((X3/current_flux)*100), (10, 420), WHITE)
-        render_text(screen, font_small, 'X4: %.2f %%' % ((X4/current_flux)*100), (10, 440), WHITE)   
+        render_text(screen, font_small, 'X4: %.2f %%' % ((X4/current_flux)*100), (10, 440), WHITE)
+        render_text(screen, font_small, 'X5: %.2f %%' % ((X5/current_flux)*100), (10, 460), WHITE)
+        render_text(screen, font_small, 'X6: %.2f %%' % ((X6/current_flux)*100), (10, 480), WHITE)
+        render_text(screen, font_small, 'X7: %.2f %%' % ((X7/current_flux)*100), (10, 500), WHITE)
+        render_text(screen, font_small, 'X8: %.2f %%' % ((X8/current_flux)*100), (10, 520), WHITE)
+
+        render_text(screen, font_normal, 'Traffic Lights', (10, 550), GREEN)
+        for  i in range(len(traffic_lights_red)):
+            render_text(screen, font_small, '%s is on red light' % traffic_lights_red[i], (10, 580 + (i*20)), RED)
+        for  i in range(len(traffic_lights_green)):
+            render_text(screen, font_small, '%s is on green light' % traffic_lights_green[i], (10, 580 + (len(traffic_lights_red)+i)*20), GREEN)
 
         # Update screen
         pg.display.flip()
+        clock.tick(FPS)  # Control the frame rate
+        frame_counter += 1
 
     pg.quit()
 
 if __name__ == "__main__":
-    run([[10, 30, 15, 25],[7, 18, 38, 17],[16, 7, 16, 11]], 50)
+    run([[27, 63, 48, 82],[35, 52, 68, 65],[48, 13, 76, 24, 72, 4, 86, 21],[]], 150)
